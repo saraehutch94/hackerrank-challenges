39d891970bc2e9f8ce0a988bb5e07d1916267dbd
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2022-05-30 14:09:01 -0400

    add plusMinus solution

--- script.js
+++ script.js
@@ -9,3 +9,28 @@ function diagonalDifference(arr) {
     }
     return Math.abs(difference);
 }
+
+// plusMinus
+
+function plusMinus(arr) {
+    let numOfPos = 0;
+    let numOfNeg = 0;
+    let numOfZero = 0;
+    let arrayLen = arr.length;
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == 0) {
+            numOfZero += 1;
+        } else if (arr[i] > 0) {
+            numOfPos += 1;
+        } else {
+            numOfNeg += 1;
+        }
+    }
+
+    numOfPos = (numOfPos / arrayLen).toFixed(6);
+    numOfNeg = (numOfNeg / arrayLen).toFixed(6);
+    numOfZero = (numOfZero / arrayLen).toFixed(6);
+
+    console.log(`${numOfPos}\n${numOfNeg}\n${numOfZero}`);
+}
